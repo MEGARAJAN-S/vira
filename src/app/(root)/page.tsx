341c2a195ffadd44766c0faaa0c159f888ff22d6
MEGARAJAN-S <megarajanmegan@gmail.com>
2025-07-21 22:28:08 +0530

    02-01: Added Background Feature Hero

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from './components/Header'
+import Hero from './components/Hero'
+import { ScrollProgress } from '@/components/modules/ScrollProgress'
 
 const page = () => {
   return (
     <div>
       <div>
         <Header />
-        
+        <ScrollProgress />
+        <Hero />
       </div>
     </div>
   )
