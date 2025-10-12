90bd39c409ef670fb9ea581137b691ac724adce9
MEGARAJAN-S <megarajanmegan@gmail.com>
2025-10-12 20:41:01 +0530

    14: meetings page setup

---
+++ src/app/dashboard/meetings/components/meetings-view.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+export const MeetingsView = () => {
+    const trpc = useTRPC();
+    const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
+
+    return (
+        <div>
+            {JSON.stringify(data)}
+        </div>
+    )
+}
+
+export const MeetingsViewLoading = () => {
+    return (
+        <LoadingState
+            title="Loading Meetings"
+            description="Fetching meetings from the database"
+        />
+    )
+}
+
+export const MeetingsViewError = () => {
+    return (
+        <ErrorState
+            title="Failed to load Meetings"
+            description="There was an error fetching meetings from the database"
+        />
+    )
+}
