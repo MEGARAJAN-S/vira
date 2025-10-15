9233200480bb93a068f70a0fc843ae8826d2041b
MEGARAJAN-S <megarajanmegan@gmail.com>
2025-10-15 22:23:24 +0530

    14-01: Meeting Form Setup

--- src/app/dashboard/meetings/page.tsx
+++ src/app/dashboard/meetings/page.tsx
@@ -3,21 +3,25 @@ import { MeetingsView, MeetingsViewError, MeetingsViewLoading } from './componen
 import { getQueryClient, trpc } from '@/trpc/server'
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import { ErrorBoundary } from 'react-error-boundary';
+import { MeetingsListHeader } from './components/meetings-list-header';
 
-const page = () => {
+const page = async () => {
     const queryClient = getQueryClient();
     void queryClient.prefetchQuery(
         trpc.meetings.getMany.queryOptions({})
     );
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<MeetingsViewLoading />}>
-            <ErrorBoundary fallback={<MeetingsViewError />}>
-                <MeetingsView />
-            </ErrorBoundary>
-        </Suspense>
-    </HydrationBoundary>
-  )
+    return (
+        <>  
+            <MeetingsListHeader />
+            <HydrationBoundary state={dehydrate(queryClient)}>
+                <Suspense fallback={<MeetingsViewLoading />}>
+                    <ErrorBoundary fallback={<MeetingsViewError />}>
+                        <MeetingsView />
+                    </ErrorBoundary>
+                </Suspense>
+            </HydrationBoundary>
+        </>
+    )
 }
 
 export default page
