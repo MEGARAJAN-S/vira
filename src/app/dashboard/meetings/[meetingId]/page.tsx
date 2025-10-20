9f8f1f8552d118d4f34ed2cd7f96de9069649868
MEGARAJAN-S <megarajanmegan@gmail.com>
2025-10-20 21:49:50 +0530

    14-04: Meeting Id Page Setup

--- src/app/dashboard/meetings/[meetingId]/page.tsx
+++ src/app/dashboard/meetings/[meetingId]/page.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { MeetingIdView, MeetingIdViewError, MeetingIdViewLoading } from "../components/meeting-id-view";
 
-const page = () => {
+interface Props {
+  params: Promise<{
+    meetingId: string;
+  }>;
+}
+
+const page = async ({ params }: Props) => {
+  const { meetingId } = await params;
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.meetings.getOne.queryOptions({ id: meetingId }),
+  );
+
   return (
-    <div>meeting Id</div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<MeetingIdViewLoading />}>
+        <ErrorBoundary fallback={<MeetingIdViewError />}>
+          <MeetingIdView meetingId={meetingId}/>
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
   )
 }
 
--- src/app/dashboard/meetings/components/new-meeting-dialog.tsx
+++ src/app/dashboard/meetings/components/new-meeting-dialog.tsx
@@ -14,7 +14,7 @@ export const NewMeetingDialog = ({ open, onOpenChange }: NewMeetingDialogProps) 
         <ResponsiveDialog title="New Meeting" description="Create New Meeting" open={open} onOpenChange={onOpenChange}>
            <MeetingForm 
            onSuccess={(id) => {onOpenChange(false); router.push(`/meetings/${id}`);}}
-           onCancel={() => onOpenChange}
+           onCancel={() => onOpenChange(false)}
         />
         </ResponsiveDialog>
     );
